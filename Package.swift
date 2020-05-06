78cb7024d00409de26db66bc63e3b27c3eeef155
Bartosz Olszanowski <bartosz.olszanowski@gmail.com>
2020-05-06 13:35:19 +0200

    Add ArgumentsParser dependency

--- Package.swift
+++ Package.swift
@@ -5,9 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "MachOReader",
+    products: [
+        .executable(name: "machoreader", targets: ["MachOReader"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
